a23a18eed2f0f7a5525711fa9f3ee9b39e5110ca
Jorge Correa <118571859+jorgeoscarcorrea@users.noreply.github.com>
2023-02-14 13:19:30 -0700

    Added a game function that lets user compete with computer for five rounds and fixed bugs

--- myscripts.js
+++ myscripts.js
@@ -1,21 +1,20 @@
 const options = ["rock", "paper", "scissors"];
 function getComputerSelection() {
-    const choice = options[Math.floor(Math.random * options.length)];
-    return choice
+    const choice = options[Math.floor(Math.random() * options.length)];
+    return choice;
 }
-
 function playRound (playerSelection, computerSelection) {
     if (
       (playerSelection == "rock" && computerSelection === "scissors") ||
       (playerSelection == "paper" && computerSelection === "rock") ||
       (playerSelection == "scissors" && computerSelection === "paper")
     ) {
-      return `You won!`;
+      return "Player Wins";
     } else if (playerSelection === computerSelection) {   
-      return `You tied!`
+      return "tie"
 
     } else {
-        return`You lost!`;
+        return"Computer Wins";
     }
 }
 
@@ -35,12 +34,33 @@ function getPlayerSelection () {
 }
 
 function game() {
-
+    let scorePlayer = 0;
+    let scoreComputer = 0
     for (let i = 0; i <5; i++) { 
         const playerSelection = getPlayerSelection();
         const computerSelection = getComputerSelection();
         console.log(playRound(playerSelection, computerSelection))
-    
+        console.log("-------")
+        if (playRound(playerSelection, computerSelection) == "Player Wins"){
+            scorePlayer++;
+            console.log(scorePlayer + "," + scoreComputer)
+        }
+        else if (playRound(playerSelection, computerSelection) == "Computer Wins"){
+            scoreComputer++;
+            console.log(scorePlayer + "," + scoreComputer)
+        }
     }
+    console.log("Gamer Over")
+    if (scorePlayer > scoreComputer){
+        console.log("Player is the winner");
+    }
+    else if (scorePlayer < scoreComputer){
+        console.log("Computer is the Winner")
+    }
+    else{
+        console.log("Try Again")
+    }
+    console.log(scorePlayer + "," + scoreComputer)
 }
     game()
+
